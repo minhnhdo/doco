336ee69cce9d82f8400d3bf804ca5e159d6b5c92
Do Nhat Minh <mrordinaire@gmail.com>
2018-03-19 15:25:08 -0700

    added hello world web server in docod

--- src/bin/docod/main.rs
+++ src/bin/docod/main.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate nickel;
+
+use nickel::Nickel;
+
 pub fn main() {
-    println!("Hello from docod!");
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            "Hello, world!"
+        }
+    });
+
+    server.listen("127.0.0.1:8080");
 }
